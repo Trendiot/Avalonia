3b65c6ee707b9b79bd51ab6a48db3e1163a4f56c
Robert Murchison <mail.rrm@pm.me>
2026-04-09 13:17:30 -0800

    Revert "Change `PostServerJob` visibility from `internal` to `public` in `Compositor`."
    This reverts commit cb8c242699a2ed43e2548fd5ce57f7bcecdbe9f3.

--- src/Avalonia.Base/Rendering/Composition/Compositor.cs
+++ src/Avalonia.Base/Rendering/Composition/Compositor.cs
@@ -245,7 +245,7 @@ namespace Avalonia.Rendering.Composition
             RequestCommitAsync();
         }
 
-        public void PostServerJob(Action job, bool postTarget = false)
+        internal void PostServerJob(Action job, bool postTarget = false)
         {
             Dispatcher.VerifyAccess();
             (postTarget ? _pendingServerCompositorPostTargetJobs : _pendingServerCompositorJobs).Add(job);
